fee2976287f55345fff78f7d886cbda8e6eb17a5
fsanperDev <fsanperdev@gmail.com>
2024-01-11 09:57:05 +0100

    nueva linea

--- UD5T1/MainPage.xaml.cs
+++ UD5T1/MainPage.xaml.cs
@@ -2,15 +2,16 @@
 {
     public partial class MainPage : ContentPage
     {
-        float cuenta;
-        int propina;
-        int personas = 1;
+        float cuenta; // Se alamacena el importe de la cuenta
+        int propina; // Se almacena el porcentaje de la propina
+        int personas = 1; // Se almacena el número de personas a dividir
 
         public MainPage()
         {
             InitializeComponent();
         }
 
+        // Metodo que calcula el total de la Cuenta
         private void CalcularTotal()
         {
             // Propina total
@@ -18,6 +19,7 @@
 
             // Propina por persona
             var propinaPorPersona = propinaTotal / personas;
+            lblPropinaPorPersona.Text = $"{propinaPorPersona:C}";
 
             // Subtotal
             var subtotal = cuenta / personas;
@@ -28,12 +30,14 @@
             lblTotal.Text = $"{totalPersonas:C}";
         }
 
+        // Metodo que recoge el importe de la cuenta y calcula el total
         private void TxtCuenta_Completa(object sender, EventArgs e)
         {
             cuenta = float.Parse(txtCuenta.Text);
             CalcularTotal();
         }
 
+        // Metodo que recoge el valor del slider y calcula el total
         private void SldPropina_ValueChanged(object sender, ValueChangedEventArgs e)
         {
             propina = Convert.ToInt32(sldPropina.Value);
@@ -41,6 +45,7 @@
             CalcularTotal();
         }
 
+        // Metodo que recoge el valor del botón y lo actualiza en el slider
         private void Button_Clicked(object sender, EventArgs e)
         {
             if(sender is Button)
@@ -51,6 +56,7 @@
             }
         }
 
+        // Metodo para disminuir el número de persona
         private void BtnMenos_Clicked(object sender, EventArgs e)
         {
             if(personas > 1)
@@ -61,6 +67,7 @@
             }
         }
 
+        // Metodo para disminuir el número de persona
         private void BtnMas_Clicked(object sender, EventArgs e)
         {
             personas++;
